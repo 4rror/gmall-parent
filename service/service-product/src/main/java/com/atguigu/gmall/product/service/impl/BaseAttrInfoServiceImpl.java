ed2f0406fb30b1a5eb0f15c31820f96982a01ee5
haisky <levlaner@foxmail.com>
2023-07-26 18:17:02 +0800

    完善保存和更新平台属性

--- service/service-product/src/main/java/com/atguigu/gmall/product/service/impl/BaseAttrInfoServiceImpl.java
+++ service/service-product/src/main/java/com/atguigu/gmall/product/service/impl/BaseAttrInfoServiceImpl.java
@@ -1,10 +1,17 @@
 package com.atguigu.gmall.product.service.impl;
 
 import com.atguigu.gmall.model.product.BaseAttrInfo;
+import com.atguigu.gmall.model.product.BaseAttrValue;
 import com.atguigu.gmall.product.mapper.BaseAttrInfoMapper;
+import com.atguigu.gmall.product.mapper.BaseAttrValueMapper;
 import com.atguigu.gmall.product.service.BaseAttrInfoService;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.CollectionUtils;
+
+import java.util.List;
 
 @Service
 public class BaseAttrInfoServiceImpl implements BaseAttrInfoService {
@@ -12,12 +19,35 @@ public class BaseAttrInfoServiceImpl implements BaseAttrInfoService {
     @Autowired
     private BaseAttrInfoMapper baseAttrInfoMapper;
 
+    @Autowired
+    private BaseAttrValueMapper baseAttrValueMapper;
+
     @Override
+    @Transactional(rollbackFor = Exception.class)
     public void saveAttrInfo(BaseAttrInfo baseAttrInfo) {
+        // 保存或修改平台属性信息
         if (baseAttrInfo.getId() != null) {
             baseAttrInfoMapper.updateById(baseAttrInfo);
         } else {
             baseAttrInfoMapper.insert(baseAttrInfo);
+        }
+
+        // 操作attrValue
+        // 先删除
+        LambdaQueryWrapper<BaseAttrValue> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(BaseAttrValue::getAttrId, baseAttrInfo.getId());
+        baseAttrValueMapper.delete(queryWrapper);
+
+        List<BaseAttrValue> attrValueList = baseAttrInfo.getAttrValueList();
+
+        // 为attrValueList的各项设置attrId
+        if (!CollectionUtils.isEmpty(attrValueList)) {
+            Long attrId = baseAttrInfo.getId();
+            attrValueList.forEach(i -> {
+                i.setAttrId(attrId);
+                // 保存
+                baseAttrValueMapper.insert(i);
+            });
         }
     }
 }
