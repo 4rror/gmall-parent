2413fade151ce0bf3646c8fceebe718457f80db3
haisky <levlaner@foxmail.com>
2023-08-16 17:45:44 +0800

    基于死信实现延迟消息

--- service/service-mq/src/main/java/com/atguigu/gmall/mq/controller/MqController.java
+++ service/service-mq/src/main/java/com/atguigu/gmall/mq/controller/MqController.java
@@ -2,10 +2,15 @@ package com.atguigu.gmall.mq.controller;
 
 import com.atguigu.gmall.common.result.Result;
 import com.atguigu.gmall.common.service.RabbitService;
+import com.atguigu.gmall.mq.config.DeadLetterMqConfig;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @RestController
 @RequestMapping("/mq")
@@ -14,6 +19,17 @@ public class MqController {
 
     @Autowired
     private RabbitService rabbitService;
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @GetMapping("/sendDeadLetter")
+    public Result<Object> sendDeadLetter() {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        this.rabbitTemplate.convertAndSend(DeadLetterMqConfig.exchange_dead, DeadLetterMqConfig.routing_dead_1, "ok");
+        System.out.println(sdf.format(new Date()) + " Delay sent.");
+        return Result.ok();
+    }
 
     /**
      * <a href="http://localhost:8282/mq/sendConfirm">http://localhost:8282/mq/sendConfirm</a>
--- service/service-mq/src/main/java/com/atguigu/gmall/mq/receiver/ConfirmReceiver.java
+++ service/service-mq/src/main/java/com/atguigu/gmall/mq/receiver/ConfirmReceiver.java
@@ -1,5 +1,6 @@
 package com.atguigu.gmall.mq.receiver;
 
+import com.atguigu.gmall.mq.config.DeadLetterMqConfig;
 import com.rabbitmq.client.Channel;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
@@ -10,9 +11,21 @@ import org.springframework.amqp.rabbit.annotation.QueueBinding;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @Slf4j
 @Component
 public class ConfirmReceiver {
+
+    @SneakyThrows
+    @RabbitListener(queues = DeadLetterMqConfig.queue_dead_2)
+    public void get(String msg, Message message, Channel channel) {
+        log.info("Receive: {}", msg);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        log.info("Receive queue_dead_2: {}. Delay receive. {}", sdf.format(new Date()), msg);
+        channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
+    }
 
     @SneakyThrows
     @RabbitListener(bindings = @QueueBinding(
